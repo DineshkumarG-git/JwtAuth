7e58a16bae2cb027dee0bfa0746b6c1acaf1d8fc
DineshkumarG-git <dineshkumar56518@gmail.com>
2023-10-28 00:57:36 +0530

    implemented userdetails interface

--- security/src/main/java/com/dinesh/security/model/User.java
+++ security/src/main/java/com/dinesh/security/model/User.java
@@ -1,17 +1,24 @@
 package com.dinesh.security.model;
 
+import com.dinesh.security.enums.Role;
 import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 @Table(name  = "_user")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,4 +27,41 @@ public class User {
     private String lastName ;
     private String email ;
     private String passWord ;
+    @Enumerated(value = EnumType.STRING) // used for mapping it will map table column type as string
+    private Role role ;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return passWord;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true  ;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true ;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true ;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
